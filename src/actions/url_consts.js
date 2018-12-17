d7f2dd12dff749a6e51c0b7abdf71658dc4ae046
oneGene <jinpark.han@gmail.com>
2018-12-17 01:46:00 +0000

    Add 'localhost' const that allows communication with localhost api server

--- src/actions/url_consts.js
+++ src/actions/url_consts.js
@@ -1,2 +1,4 @@
 // export const BASEURL = 'http://127.0.0.1:8018'
-export const BASEURL = 'http://a349368d.ngrok.io'
+// export const BASEURL = 'http://20c3ad04.ngrok.io'
+// export const BASEURL = 'http://192.168.1.161:8018'
+export const BASEURL = 'http://10.0.2.2:8018'
